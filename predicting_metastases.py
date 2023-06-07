c09a5cf9efb1b7cb2f0b49093dae6c378aa625e4
hilleld <hillel.darshan@mail.huji.ac.il>
2023-06-07 22:48:13 +0300

    predictor - hillel init

--- predicting_metastases.py
+++ predicting_metastases.py
@@ -1,10 +1,15 @@
 from sklearn.base import BaseEstimator
 class PredictingMetastases:
     def __int__(self):
-        pass
+        self.logistic_rearners_ = []
 
-    def _predict(self):
-        pass
+
 
     def _fit(self):
+        pass
+
+
+
+
+    def _predict(self):
         pass
