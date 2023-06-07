5082deda40c6932fd957b6e2f245e5f633b8a6d2
Itamarshahar <78688846+Itamarshahar@users.noreply.github.com>
2023-06-07 22:12:33 +0300

    data-commot

--- pre_process.py
+++ pre_process.py
@@ -28,7 +28,7 @@ def load_data(samples_file_name: str, responses_file_name: str):
     # load data
     raw_data_x = pd.read_csv(samples_file_name)
     raw_data_y = pd.read_csv(responses_file_name)
-    X_train, X_test, y_train, y_test = train_test_split(raw_data_x, raw_data_y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(raw_data_x, raw_data_y,test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
 def prepreprocess(X_train: pd.DataFrame, y_train: pd.DataFrame ):
