496fc2a5952a4a2fe6154dfaa22a835ed06c1004
Itamarshahar <78688846+Itamarshahar@users.noreply.github.com>
2023-06-07 22:49:22 +0300

    data-commot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import pre_process
+from pre_process import run_preprocess
 if __name__ == '__main__':
     cols_to_remove = []
-    pre_process.run_preprocess("./train.feats.csv", "./train.labels.0.csv", cols_to_remove)
+    link1 = "./train.feats.csv"
+    link2 = "./train.labels.0.csv"
+    run_preprocess(link1, link2, cols_to_remove)
 
 
