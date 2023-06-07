a9fb9ed8715bc534f3517e93cbe8f06be8360717
Itamarshahar <78688846+Itamarshahar@users.noreply.github.com>
2023-06-07 22:41:33 +0300

    data-commot

---
+++ predicting_metastases.py
@@ -0,0 +1,10 @@
+from sklearn.base import BaseEstimator
+class PredictingMetastases:
+    def __int__(self):
+        pass
+
+    def _predict(self):
+        pass
+
+    def _fit(self):
+        pass
