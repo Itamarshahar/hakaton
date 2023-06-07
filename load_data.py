13638b06788872b1efba903e103b2bc3547b85e5
itanini <itamar.nini@mail.huji.ac.il>
2023-06-07 21:49:48 +0300

    split data initial

--- load_data.py
+++ load_data.py
@@ -24,10 +24,14 @@ def load_data(samples_file_name: str, responses_file_name: str):
     """
 
 
-    # load data and drop unnecessary data
+    # load data
     raw_data_x = pd.read_csv(samples_file_name)
     raw_data_y = pd.read_csv(responses_file_name)
     X_train, X_test, y_train, y_test = train_test_split(raw_data_x, raw_data_y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
+
+def preprocess(X_train: ndarray)
+
+
 
 
