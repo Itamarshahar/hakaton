c5c1ec2fdf771dde6bfabac9bdddc3eeedd4205b
itanini <itamar.nini@mail.huji.ac.il>
2023-06-07 22:01:31 +0300

    loaded data checked - Dataframe it is

--- load_data.py
+++ load_data.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 
 
 
-def load_data(samples_file_name: str, responses_file_name: str):
+def load_data_func(samples_file_name: str, responses_file_name: str):
     """
     Load house prices dataset and preprocess data.
     Parameters
@@ -30,8 +30,9 @@ def load_data(samples_file_name: str, responses_file_name: str):
     X_train, X_test, y_train, y_test = train_test_split(raw_data_x, raw_data_y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
-def preprocess(X_train: ndarray)
 
+def preprocess():
+    pass
 
 
 
