fba04cbf57a3add11d14527626c4bffc9547efc4
Itamarshahar <78688846+Itamarshahar@users.noreply.github.com>
2023-06-08 17:09:20 +0300

    comm

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 
 from pre_process import run_preprocess, make_unique_response
 from visualization import draw, catagorial_label_perc
+from pca_visualization import run_pca_visualisation
 import predicting_tumor_size
 SAMPLE_PATH_10 = "./Data/DATA_by_percent_THIS_IS_GOOD/10_percent_train/10_train.feats.csv"
 LABEL_PATH_10 = "./Data/DATA_by_percent_THIS_IS_GOOD/10_percent_train/10_train.labels.0.csv"
@@ -15,7 +16,7 @@ COLS_TO_DUM = ['FormName','Basicstage', 'Hospital',
 
 COL_TO_REMOVE = ['Diagnosisdate', 'Surgerydate1', 'Surgerydate2','Surgerydate3','surgerybeforeorafter-Activitydate']
 
-def testing_tumor_size(str1, str2, lst1, lst2):
+def tumor_size(str1, str2, lst1, lst2):
     hillel_X, hillel_y = run_preprocess(str1, str2, lst1, lst2, mode="tumor_size")
     h_train_x, h_train_y, h_test_x, h_test_y = train_test_split(hillel_X, hillel_y, test_size=0.2)
     learner = predicting_tumor_size.PredictTumorSize()
@@ -28,7 +29,7 @@ if __name__ == '__main__':
     #run_preprocess("./train.feats.csv", "./train.labels.0.csv", cols_to_remove)
     cols_to_remove = []
     X, y = run_preprocess(SAMPLE_PATH_20, LABEL_PATH_20,COL_TO_REMOVE, COLS_TO_DUM)
-    print(testing_tumor_size(SAMPLE_PATH_20, LABEL_PATH_20,COL_TO_REMOVE, COLS_TO_DUM))
-    catagorial_label_perc(X, y, )
-    draw(X, make_unique_response(y))
-
+    # print(testing_tumor_size(SAMPLE_PATH_20, LABEL_PATH_20,COL_TO_REMOVE, COLS_TO_DUM))
+    # catagorial_label_perc(X, y, )
+    # draw(X, make_unique_response(y))
+    run_pca_visualisation(X,y)
