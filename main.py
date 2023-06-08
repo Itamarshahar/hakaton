e467795ed097ccd492266b02fc5922f1bb4d2a2c
Itamarshahar <78688846+Itamarshahar@users.noreply.github.com>
2023-06-08 21:45:47 +0300

    comm

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from visualization import draw, catagorial_label_perc, generate_is_sick_vector
 import predicting_tumor_size
 from predicting_metastases import PredictingMetastases
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+import plotly.express as px
 import scipy.stats as stats
 SAMPLE_PATH_10 = "./Data/DATA_by_percent_THIS_IS_GOOD/10_percent_train/10_train.feats.csv"
 LABEL_PATH_10 = "./Data/DATA_by_percent_THIS_IS_GOOD/10_percent_train/10_train.labels.0.csv"
@@ -43,7 +45,32 @@ def run_metastases(X,y):
     print(loss)
 
 
+def model_best_features(x, y):
+    rf_classifier = RandomForestClassifier(n_estimators=100)
+    rf_classifier.fit(x, y)
+    feature_importances = rf_classifier.feature_importances_
+    feature_names = x.columns
+    feature_importance_dict = dict(zip(feature_names, feature_importances))
+    sorted_feature_importances = sorted(feature_importance_dict.items(),
+                                        key=lambda x: x[1], reverse=True)
 
+    feature_names_sorted = []
+    feature_importances_sorted = []
+    for feature, importance in sorted_feature_importances:
+        feature_names_sorted.append(feature)
+        feature_importances_sorted.append(importance)
+        print(f"Feature: {feature}, Importance: {importance}")
+
+    fig = px.bar(x=feature_names_sorted, y=feature_importances_sorted, title='Features importance using model weights')
+    fig.show()
+
+
+        # fig = px.bar(sorted_feature_importances, x='Feature', y='Correlation',
+        #          title='Feature importance')
+    #
+    # fig.show()
+
+    return sorted_feature_importances
 
 if __name__ == '__main__':
     np.random.seed(0)
@@ -55,6 +82,9 @@ if __name__ == '__main__':
 
     # X, y = run_preprocess("/Users/itamar_shahar/PycharmProjects/hakaton/Data/original_data_DONT_TUOCH!!!/train.feats.csv", "/Users/itamar_shahar/PycharmProjects/hakaton/Data/original_data_DONT_TUOCH!!!/train.labels.0.csv",COL_TO_REMOVE, COLS_TO_DUM)
     X, y = run_preprocess(SAMPLE_PATH_60, LABEL_PATH_60,COL_TO_REMOVE, COLS_TO_DUM)
+    model_best_features(X,y)
+
+
     run_metastases(X,y)
     # run_tumor_size(SAMPLE_PATH_60, LABEL_PATH_60,COL_TO_REMOVE, COLS_TO_DUM)
     # for col in COLS_TO_DUM:
