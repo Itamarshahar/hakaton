3dd5c4bb7a14ebdabce47bf7edfb22d768057c44
itanini <itamar.nini@mail.huji.ac.il>
2023-06-08 12:00:36 +0300

    cange value works!!

--- pre_process.py
+++ pre_process.py
@@ -56,10 +56,11 @@ def prepreprocess(X_train: pd.DataFrame, y_train: pd.DataFrame, cols_to_remove: 
 
 def change_value(df : pd.DataFrame, col_name:str , convert_dict: dict[str,int], default_value: any):
     look_for_key = convert_dict.keys()
-    col = df[col_name]
+    col = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)[col_name]
     for key in look_for_key:
-        col = col.replace({r'.*{}.'.format(key): convert_dict[key]}, regex = True)
+        col = col.replace({r'{}'.format(key)}, convert_dict[key] ,regex = True)
     df[col_name] = col
+    return df
 def convert_to_dummies(X_train, col_to_dummies, splitter:str = "+"):
     X_train[col_to_dummies] = X_train[col_to_dummies].str.replace(' ', '_')
     unique_words = set()
@@ -80,13 +81,15 @@ def make_unique_response(responses: pd.DataFrame) -> pd.DataFrame:
 def clean_responses(reponse:str):
     matches = re.findall(r"'(.*?)'", reponse)
     return ','.join(matches)
-def run_preprocess(samples_file_name: str, responses_file_name: str, cols_to_remove:[str], cols_to_dummies:[str]) ->:
+def run_preprocess(samples_file_name: str, responses_file_name: str, cols_to_remove:[str], cols_to_dummies:[str]):
     """
     return matrix of only numbers
     """
     X_train, X_test, y_train, y_test = load_data(samples_file_name, responses_file_name)
     X_train = prepreprocess(X_train, y_train, cols_to_remove, cols_to_dummies)
     make_unique_response(y_train)
+    er_dict = {'pos':99999}
+    df = change_value(X_train, 'er', er_dict, 555555)
     non_numeric_cols = X_train.select_dtypes(exclude=[np.number]).columns
     X_train_numeric_only = X_train.drop(non_numeric_cols, axis=1)
     X_train_numeric_only.fillna(0, inplace=True)
